55543fa183ccf36fdfcee03b61ca5bac8131efff
Rylan Graham <95762889+RylanJGraham@users.noreply.github.com>
2024-05-13 21:40:45 +0200

    CookBook & Settings

--- CulinARy/Assets/UserCookBook.cs
+++ CulinARy/Assets/UserCookBook.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using System; // Add this line to include the System namespace
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Firebase.Auth;
 
 public class UserCookBook : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TMP_Text displayNameText;
+
     void Start()
     {
-        
-    }
+        // Get the FirebaseAuth instance
+        FirebaseAuth auth = FirebaseAuth.DefaultInstance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Check if a user is currently logged in
+        if (auth.CurrentUser != null)
+        {
+            // Access the currently logged-in user
+            FirebaseUser user = auth.CurrentUser;
+
+            // Log the user's data
+            Debug.Log("User ID: " + user.UserId);
+            Debug.Log("User Email: " + user.Email);
+            Debug.Log("User Display Name: " + user.DisplayName);
+            
+            // Update display name text
+            displayNameText.text = user.DisplayName + "'s";
+        }
+        else
+        {
+            // If no user is logged in, log a message
+            Debug.Log("No user is currently logged in.");
+        }
     }
 }
